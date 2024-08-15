9c3d0111697d50e30ea079bf537200641b985686
foolishchow <foolishchow@dingtail.com>
2024-08-15 17:29:47 +0800

    @chore update

--- element/docs/.vitepress/theme/element-fix/ProvidedLayot.tsx
+++ element/docs/.vitepress/theme/element-fix/ProvidedLayot.tsx
@@ -3,7 +3,7 @@ import { SetupContext } from 'vue'
 import Layout from 'vitepress-theme-document/src/Layout.vue'
 import 'vitepress-theme-document/src/custom.css'
 import { ElConfigProvider } from 'element-plus'
-import zhCN from 'element-plus/lib/locale/lang/zh-cn'
+import zhCN from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 import './style-fix.css'
 export default function ElementPlusProvidedLayout(props: any, context: SetupContext) {
